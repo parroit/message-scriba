8a703910644b57f01baf0b3732335776d5594745
parroit <andrea.parodi@ebansoftware.net>
2013-10-13 09:27:46 +0200

    args checking

--- test/message-scriba_test.js
+++ test/message-scriba_test.js
@@ -1,36 +1,89 @@
+/*
+ * message-scriba
+ * https://github.com/parroit/message-scriba
+ *
+ * Copyright (c) 2013 parroit
+ * Licensed under the MIT license.
+ */
+
 'use strict';
 
 var message_scriba = require('../lib/message-scriba.js');
+var Scriba = message_scriba.Scriba;
+var scriba = new Scriba();
 
-/*
-  ======== A Handy Little Nodeunit Reference ========
-  https://github.com/caolan/nodeunit
+var expect = require('chai').expect;
+require('chai').should();
 
-  Test methods:
-    test.expect(numAssertions)
-    test.done()
-  Test assertions:
-    test.ok(value, [message])
-    test.equal(actual, expected, [message])
-    test.notEqual(actual, expected, [message])
-    test.deepEqual(actual, expected, [message])
-    test.notDeepEqual(actual, expected, [message])
-    test.strictEqual(actual, expected, [message])
-    test.notStrictEqual(actual, expected, [message])
-    test.throws(block, [error], [message])
-    test.doesNotThrow(block, [error], [message])
-    test.ifError(value)
-*/
 
-exports['awesome'] = {
-  setUp: function(done) {
-    // setup here
-    done();
-  },
-  'no args': function(test) {
-    test.expect(1);
-    // tests here
-    test.equal(message_scriba.awesome(), 'awesome', 'should be awesome.');
-    test.done();
-  },
-};
+describe('messageParser',function(){
+    describe("module",function() {
+        it("should load",function(){
+            expect(message_scriba).not.to.be.equal(null);
+            expect(message_scriba).to.be.a('object');
+
+        });
+    });
+
+    it("should throw on non string db path",function(){
+        (function () {
+            scriba.run(null,'');
+        }).should.throw(Error);
+
+        (function () {
+            scriba.run(undefined,'');
+        }).should.throw(Error);
+
+        (function () {
+            scriba.run(true,'');
+        }).should.throw(Error);
+
+        (function () {
+            scriba.run(/ /,'');
+        }).should.throw(Error);
+
+        (function () {
+            scriba.run(42,'');
+        }).should.throw(Error);
+
+        (function () {
+            scriba.run({},'');
+        }).should.throw(Error);
+
+        (function () {
+            scriba.run(function(){},'');
+        }).should.throw(Error);
+
+    });
+
+    it("should throw on non string attachment path",function(){
+        (function () {
+            scriba.run('db/path',null);
+        }).should.throw(Error);
+
+        (function () {
+            scriba.run('db/path',undefined);
+        }).should.throw(Error);
+
+        (function () {
+            scriba.run('db/path',true);
+        }).should.throw(Error);
+
+        (function () {
+            scriba.run('db/path',/ /);
+        }).should.throw(Error);
+
+        (function () {
+            scriba.run('db/path',42);
+        }).should.throw(Error);
+
+        (function () {
+            scriba.run('db/path',{});
+        }).should.throw(Error);
+
+        (function () {
+            scriba.run('db/path',function(){});
+        }).should.throw(Error);
+
+    });
+});    
